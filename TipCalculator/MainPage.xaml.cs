ef6fcf6852bf608e38ef057688f70548d2d9089b
Fernando Gomes <fefegomes040108@gmail.com>
2024-08-23 21:37:11 -0300

    criei o projeto

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void sethighTip0_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setLowTTip_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundUpButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundDownButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
+
